45d0291adeb1c4bfd73fd7a9c7d58194c6b14eda
Danil Popov <zabastx@gmail.com>
2022-05-06 00:07:08 +0300

    authpage: composition api ts

--- client/src/views/AuthPage.vue
+++ client/src/views/AuthPage.vue
@@ -4,23 +4,23 @@
 		<h2>Авторизация</h2>
 		<form class="auth-form" @submit.prevent>
 			<label class="input input-label input-login">
-				<input type="text" name="username" v-model="username" class="input input-text" required>
-				<span :class="{ active: username, placeholder: true }">Логин</span>
+				<input type="text" name="username" v-model="state.username" class="input input-text" required>
+				<span :class="{ active: state.username, placeholder: true }">Логин</span>
 			</label>
 			<label class="input input-label input-pass">
-				<input type="password" name="password" v-model="password" class="input input-text" required>
-				<span :class="{ active: password, placeholder: true }">Пароль</span>
+				<input type="password" name="password" v-model="state.password" class="input input-text" required>
+				<span :class="{ active: state.password, placeholder: true }">Пароль</span>
 			</label>
 			<div class="buttons">
-				<button :disabled="inProcess" class="btn login" @click="handleLog">
+				<button :disabled="state.inProcess" class="btn login" @click="handleLog">
 					Войти
 				</button>
-				<button :disabled="inProcess" class="btn register" @click="handleReg">
+				<button :disabled="state.inProcess" class="btn register" @click="handleReg">
 					Регистрация
 				</button>
 			</div>
 		</form>
-		<Loading :active="inProcess" :is-full-page="true" :opacity="0" color="#6e14ef" />
+		<Loading :active="state.inProcess" :is-full-page="true" :opacity="0" color="#6e14ef" />
 	</main>
 </template>
 
@@ -58,96 +58,79 @@ h1 {
 }
 </style>
 
-<script>
+<script lang="ts" setup>
 import { myToast } from '../utils'
 import Loading from 'vue-loading-overlay'
 import 'vue-loading-overlay/dist/vue-loading.css'
+import { reactive } from 'vue'
+import axios, { AxiosResponse } from 'axios'
+import { useRouter } from 'vue-router'
 
-export default {
-	name: 'Auth',
-	components: {
-		Loading
-	},
-	data() {
-		return {
-			username: '',
-			password: '',
-			inProcess: false
-		}
-	},
+interface LogData {
+	username: string
+	token: string
+	userId: string
+}
 
-	methods: {
-		async authSubmit(event) {
-			if (event.submitter.className.includes('login')) {
-				await this.handleLog()
-				return
-			}
+const emit = defineEmits<{
+	(e: 'login', data: LogData): void
+}>()
 
-			await this.handleReg()
-		},
+const state = reactive({
+	username: '',
+	password: '',
+	inProcess: false
+})
 
-		async handleLog() {
-			try {
-				this.inProcess = true
-				const response = await fetch('/api/auth/login', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({
-						username: this.username.trim().toLowerCase(),
-						password: this.password.trim().toLowerCase()
-					})
-				})
+interface LogResponse {
+	token: string
+	userId: string
+	username: string
+}
 
-				const data = await response.json()
+const router = useRouter()
 
-				if (!response.ok) throw Error(data.message)
+const handleLog = async (): Promise<void> => {
+	state.inProcess = true
+	try {
+		const username = state.username.trim().toLocaleLowerCase()
+		const password = state.password.trim().toLocaleLowerCase()
 
-				localStorage.setItem('token', data.token)
-				localStorage.setItem('username', data.username)
+		const res: AxiosResponse<LogResponse> = await axios.post('/api/auth/login', { username, password })
 
-				this.$emit('login', data)
+		localStorage.setItem('token', res.data.token)
+		localStorage.setItem('username', res.data.username)
 
-				this.$router.removeRoute('Auth')
-				this.$router.push('/create')
+		emit('login', res.data)
 
-			} catch (e) {
-				myToast({
-					text: e.message,
-					type: 'danger'
-				})
-			}
-			this.inProcess = false
-		},
-		async handleReg() {
-			try {
-				this.inProcess = true
-
-				const response = await fetch('/api/auth/register', {
-					method: 'POST',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body: JSON.stringify({
-						username: this.username.trim().toLowerCase(),
-						password: this.password.trim().toLowerCase()
-					})
-				})
-
-				const data = await response.json()
-
-				if (!response.ok) throw Error(data.message)
-
-				await this.handleLog()
-			} catch (e) {
-				myToast({
-					text: e.message,
-					type: 'danger'
-				})
-			}
-			this.inProcess = false
-		},
+		router.removeRoute('Auth')
+		router.push('/create')
+	} catch (e) {
+		myToast({
+			text: (e as Error).message,
+			type: 'danger'
+		})
 	}
+	state.inProcess = false
+}
+
+const handleReg = async (): Promise<void> => {
+	state.inProcess = true
+	try {
+		const username = state.username.trim().toLocaleLowerCase()
+		const password = state.password.trim().toLocaleLowerCase()
+
+		const res: AxiosResponse<{ message: string }> = await axios.post('/api/auth/register', { username, password })
+
+		if (res.status !== 200) throw new Error(res.data.message)
+
+		await handleLog()
+	} catch (e) {
+		myToast({
+			text: (e as Error).message,
+			type: 'danger'
+		})
+	}
+	state.inProcess = false
 }
 </script>
